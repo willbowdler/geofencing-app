ee486abcbd390dcce269c0b9c7424792f6721320
Will Bowdler <willbowdler@Wills-MacBook-Pro.local>
2022-07-23 14:30:24 -0500

    beginning work on session authentication. login route. setting cookies

--- server/app.js
+++ server/app.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 const PORT = process.env.PORT || 3001
 const app = express()
 
-const auth = require('./routes/auth')
+const authRoutes = require('./routes/auth')
 
 mongoose.connect('mongodb://localhost:27017/geofencing-db')
 
 app.use(bodyParser.json())
-app.use('/api/auth', auth)
+app.use(cookieParser())
+app.use('/api/auth', authRoutes)
 
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from server!' })
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,20 +5,40 @@ const bcrypt = require('bcrypt')
 const User = require('../models/userModel')
 
 router.post('/register', async (req, res) => {
-  const hashedPass = await bcrypt.hash(req.body.pass, 15)
-  const newData = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    pass: hashedPass,
-  }
-  const newUser = new User(newData)
-  newUser.save((err) => {
-    err && console.log(err.message)
-  })
+  try {
+    const hashedPass = await bcrypt.hash(req.body.pass, 10)
+    const newData = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      pass: hashedPass,
+    }
+    const newUser = new User(newData)
+    newUser.save((err) => {
+      err && console.log(err.message)
+    })
 
-  console.log(newData)
-  res.send('it worked')
+    console.log(newData)
+    res.send('it worked')
+  } catch (error) {
+    res.status(500).send()
+  }
+})
+
+router.post('/login', async (req, res) => {
+  const user = '' // add mongoose functionality that looks for the user based on email
+  try {
+    if (await bcrypt.compare(req.body.pass, user.pass)) {
+      res.send('This user exists and this is the right password')
+    } else {
+      res.send('Unsuccessful')
+    }
+    // then give them permission to access the dashboard and then route them to the dashboard
+  } catch (error) {}
+})
+
+router.post('/logout', async (req, res) => {
+  res.end()
 })
 
 module.exports = router
